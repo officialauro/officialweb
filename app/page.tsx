0e87eb67769d21b66996f0493b8713f984962bc8
officialauro <77210074+officialauro@users.noreply.github.com>
2024-01-01 21:49:00 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,6 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Add our bot{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 font-size:25px`}>
